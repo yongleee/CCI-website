c96360f29c47850f6d969802987231330a0d2499
Your Name <leeyl9797@gmail.com>
2022-05-18 07:08:35 +0800

    adding title for home page

--- src/router.js
+++ src/router.js
@@ -5,6 +5,9 @@ const routes = [
     path: "/",
     name: "Home",
     component: () => import("./views/Home.vue"),
+    meta: {
+      title: "主页",
+    },
   },
   {
     path: "/:id",
@@ -17,4 +20,9 @@ const routes = [
 export const router = createRouter({
   routes,
   history: createWebHistory(import.meta.env.BASE_URL),
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
