a077a5230f00de9afbd744d2547dc346f421cf51
Your Name <leeyl9797@gmail.com>
2022-05-23 19:48:51 +0800

    adding catchall and redirecting to home

--- src/router.js
+++ src/router.js
@@ -15,6 +15,10 @@ const routes = [
     component: () => import("./views/Content.vue"),
     props: true,
   },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/",
+  },
 ];
 
 export const router = createRouter({
